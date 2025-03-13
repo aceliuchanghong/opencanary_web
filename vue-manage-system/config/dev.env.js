b611604c9a11488d356699960f52e69887b7ce37
lch <aceliuchanghong@gmail.com>
2025-03-13 18:12:41 +0800

    feat: 删除不需要的部分

--- vue-manage-system/config/dev.env.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-
-module.exports = merge(prodEnv, {
-  NODE_ENV: '"development"',
-  // API_HOST:'"http://172.18.214.121:80"'
-  API_HOST:'"http://192.168.1.11:80"'
-})
